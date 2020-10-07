4ea9b2ce3c377f7584ff8aa5e06fa24f0ff89f35
Omar Abdelhamid Khaled <omardemiro@gmail.com>
2020-10-07 11:25:46 +0200

    Adding test functions

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -34,6 +34,88 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def testGetCategories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
+    def testGetQuestions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+        self.assertTrue(len(data['questions']))
+        self.assertIsNotNone(data['total_questions'])
+
+    def testDeleteQuestion(self):
+        question = Question(question='Test Question', 
+                            answer='Test Answer',
+                            difficulty=1, 
+                            category=1)
+        question.insert()
+        question_id = question.id
+        res = self.client().delete(f'/questions/{question_id}')
+        data = json.loads(res.data)
+        question = Question.query.filter(
+            Question.id == question.id).one_or_none()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], str(question_id))
+        self.assertEqual(question, None)
+
+    def testCreateQuestion(self):
+        new_question = {
+            'question': 'Test question',
+            'answer': 'New Answer',
+            'difficulty': 1,
+            'category': 1
+        }
+        total_questions_before = len(Question.query.all())
+        res = self.client().post('/questions', json=new_question)
+        data = json.loads(res.data)
+        total_questions_after = len(Question.query.all())
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(total_questions_after, total_questions_before + 1)
+
+    def testSearchQuestions(self):
+        new_search = {
+            'searchTerm': 'title'
+        }
+        res = self.client().post('/questions/search', json=new_search)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsNotNone(data['questions'])
+        self.assertIsNotNone(data['total_questions'])
+        self.assertIsNotNone(data['current_category'], None)
+
+    def testGetCategoryQuestions(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+        self.assertIsNotNone(data['current_category'])
+        self.assertTrue(len(data['questions']))
+        self.assertIsNotNone(data['total_questions'])
+
+    def testPlayQuiz(self):
+        new_round = {
+            'previous_questions': [],
+            'quiz_category': {
+                'type': 'Science', 
+                'id': 1
+            }
+        }
+        res = self.client().post('/quizzes', json=new_round)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsNotNone(data['question'])
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
